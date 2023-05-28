257a11f58f14ef3abf9dbf754716a62a8790be08
nogu66 <ezochi66@gmail.com>
2023-05-28 22:08:53 +0900

    Initial Commit

---
+++ TwitterSwiftUI/TwitterSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwitterSwiftUIApp.swift
+//  TwitterSwiftUI
+//
+//  Created by YutaNoguchi on 2023/05/28.
+//
+
+import SwiftUI
+
+@main
+struct TwitterSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
