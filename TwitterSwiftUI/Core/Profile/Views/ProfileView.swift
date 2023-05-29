50cdb42d57a6dd71e0c8541356b8ba0f95d7a86c
nogu66 <ezochi66@gmail.com>
2023-05-29 20:07:50 +0900

    プロフィール部分の作成

---
+++ TwitterSwiftUI/Core/Profile/Views/ProfileView.swift
@@ -0,0 +1,132 @@
+//
+//  ProfileView.swift
+//  TwitterSwiftUI
+//
+//  Created by YutaNoguchi on 2023/05/29.
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+    var body: some View {
+        VStack(alignment: .leading) {
+            headerView
+            
+            actionButton
+            
+            userInfoDetails
+            
+            Spacer()
+        }
+    }
+}
+
+struct ProfileView_Previews: PreviewProvider {
+    static var previews: some View {
+        ProfileView()
+    }
+}
+
+
+extension ProfileView {
+    
+    var headerView: some View {
+        ZStack(alignment: .bottomLeading) {
+            Color(.systemBlue)
+                .ignoresSafeArea()
+            VStack {
+                Button {
+                    
+                } label: {
+                    Image(systemName: "arrow.left")
+                        .resizable()
+                        .frame(width: 20, height: 16)
+                        .foregroundColor(.white)
+                        .offset(x: 15, y: 12)
+                }
+                
+                Circle()
+                    .frame(width: 72, height: 72)
+                    .offset(x: 16, y: 24)
+            }
+        }
+        .frame(height: 96)
+    }
+    
+    var actionButton: some View {
+        HStack {
+            Spacer()
+            
+            Image(systemName: "bell.badge")
+                .font(.title3)
+                .padding(6)
+                .overlay(Circle().stroke(Color.gray, lineWidth: 0.75))
+            
+            Button {
+                
+            } label: {
+                Text("Edit Profile")
+                    .font(.subheadline).bold()
+                    .frame(width: 120, height: 32)
+                    .overlay(RoundedRectangle(cornerRadius: 20).stroke( Color.gray, lineWidth: 0.75))
+            }
+        }
+        .padding(.trailing)
+    }
+    
+    var userInfoDetails: some View {
+        VStack(alignment: .leading, spacing: 4) {
+            HStack {
+                Text("Heath Ledger")
+                    .font(.title2).bold()
+                
+                Image(systemName: "checkmark.seal.fill")
+                    .foregroundColor(Color(.systemBlue))
+            }
+            
+            Text("@joker")
+                .font(.subheadline)
+                .foregroundColor(.gray)
+            
+            Text("Your moms favorite villain")
+                .font(.subheadline)
+                .padding(.vertical)
+            
+            HStack(spacing: 32) {
+                HStack {
+                    Image(systemName: "mappin.and.ellipse")
+                    Text("Gotham, NY")
+                }
+                
+                HStack {
+                    Image(systemName: "link")
+                    
+                    Text("www.thejoker.com")
+                    
+                }
+            }
+            .font(.caption)
+            .foregroundColor(.gray)
+            
+            HStack(spacing: 24) {
+                HStack(spacing: 4) {
+                    Text("807")
+                        .font(.subheadline)
+                        .bold()
+                    Text("Following")
+                        .font(.caption)
+                }
+                
+                HStack {
+                    Text("6.9<")
+                        .font(.subheadline)
+                        .bold()
+                    Text("Followers")
+                        .font(.caption)
+                }
+            }
+            .padding(.vertical)
+        }
+        .padding(.horizontal)
+    }
+}
