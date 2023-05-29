f5840df4b15d161045aa0c5615b37d496cb47421
nogu66 <ezochi66@gmail.com>
2023-05-29 13:30:34 +0900

    TabViewを使用

---
+++ TwitterSwiftUI/Core/Message/MessageView.swift
@@ -0,0 +1,20 @@
+//
+//  MessageView.swift
+//  TwitterSwiftUI
+//
+//  Created by YutaNoguchi on 2023/05/29.
+//
+
+import SwiftUI
+
+struct MessageView: View {
+    var body: some View {
+        Text("Message View")
+    }
+}
+
+struct MessageView_Previews: PreviewProvider {
+    static var previews: some View {
+        MessageView()
+    }
+}
---
+++ TwitterSwiftUI/Core/Notifications/NotificationsView.swift
@@ -0,0 +1,20 @@
+//
+//  NotificationsView.swift
+//  TwitterSwiftUI
+//
+//  Created by YutaNoguchi on 2023/05/29.
+//
+
+import SwiftUI
+
+struct NotificationsView: View {
+    var body: some View {
+        Text("Notifications View")
+    }
+}
+
+struct NotificationsView_Previews: PreviewProvider {
+    static var previews: some View {
+        NotificationsView()
+    }
+}
