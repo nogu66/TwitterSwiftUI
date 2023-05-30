efd3806e63db51d040cc72223ba9dd5c18ef20ce
nogu66 <ezochi66@gmail.com>
2023-05-30 21:08:45 +0900

    ログインページを実装

---
+++ TwitterSwiftUI/Core/Authentication/View/LoginView.swift
@@ -0,0 +1,96 @@
+//
+//  LoginView.swift
+//  TwitterSwiftUI
+//
+//  Created by YutaNoguchi on 2023/05/30.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    @State private var email = ""
+    @State private var password = ""
+    var body: some View {
+        // parent container
+        VStack {
+            // header view
+            VStack(alignment: .leading) {
+                HStack {Spacer()}
+                
+                Text("Hello.")
+                    .font(.largeTitle)
+                    .fontWeight(.semibold)
+                
+                Text("Welcom Back")
+                    .font(.largeTitle)
+                    .fontWeight(.semibold)
+            }
+            .frame(height:240)
+            .padding(.leading)
+            .background(Color(.systemBlue))
+            .foregroundColor(.white)
+            .clipShape(RoundedShape(corners: [.bottomRight]))
+            
+            VStack(spacing: 40) {
+                CustomInputField(imageName: "envelope", placeholderText: "Email", text: $email)
+                CustomInputField(imageName: "lock", placeholderText: "Password", text: $password)
+            }
+            .padding(.horizontal, 32)
+            .padding(.top, 44)
+            
+            HStack {
+                Spacer()
+                
+                NavigationLink {
+                    Text("Reset password view..")
+                } label: {
+                    Text("Forget Password")
+                        .font(.caption)
+                        .fontWeight(.bold)
+                        .foregroundColor(Color(.systemBlue))
+                        .padding(.top)
+                        .padding(.trailing, 24)
+                }
+            }
+            
+            Button {
+                print("Sign in here..")
+            } label: {
+                Text("Sign In")
+                    .font(.headline)
+                    .foregroundColor(.white)
+                    .frame(width: 340, height: 50)
+                    .background(Color(.systemBlue))
+                    .clipShape(Capsule())
+                    .padding()
+            }
+            .shadow(color: .gray.opacity(0.5), radius: 10, x: 0, y: 0)
+            
+            Spacer()
+            
+            NavigationLink {
+                RegistrationView()
+                    .navigationBarHidden(true)
+            } label: {
+                HStack {
+                    Text("Don't have an acount?")
+                        .font(.footnote)
+                    
+                    Text("Sign Up")
+                        .font(.footnote)
+                        .fontWeight(.semibold)
+                }
+            }
+            .padding(.bottom, 32)
+            .foregroundColor(Color(.systemBlue))
+        }
+        .ignoresSafeArea()
+        .navigationBarHidden(true)
+    }
+}
+
+struct LoginView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginView()
+    }
+}
---
+++ TwitterSwiftUI/Core/Components/RoundedShape.swift
@@ -0,0 +1,18 @@
+//
+//  RoundedShape.swift
+//  TwitterSwiftUI
+//
+//  Created by YutaNoguchi on 2023/05/30.
+//
+
+import SwiftUI
+
+struct RoundedShape: Shape {
+    var corners: UIRectCorner
+    
+    func path(in rect: CGRect) -> Path {
+        let path = UIBezierPath(roundedRect: rect, byRoundingCorners: corners, cornerRadii: CGSize(width: 80, height: 80))
+        
+        return Path(path.cgPath)
+    }
+}
